e654e2608b785613adf3143db7e02876887891a6
Daniel Connelly <dhconnelly@gmail.com>
2024-05-06 21:26:47 +0200

    404

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,12 @@
 import static java.nio.charset.StandardCharsets.US_ASCII;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.regex.Pattern;
 
 public class Main {
     private static final void die(Exception e) {
@@ -10,16 +14,41 @@ public class Main {
         System.exit(1);
     }
 
+    private static final Pattern REQUEST_LINE_PATTERN = Pattern.compile("GET (\\S+) HTTP\\/1.1");
+
     static void handle(Socket client) throws IOException {
-        try (client) {
+        var lines = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        var requestLine = lines.readLine();
+        System.out.printf("request: [%s]\n", requestLine);
+        var matcher = REQUEST_LINE_PATTERN.matcher(requestLine);
+        if (!matcher.matches()) {
+            client.getOutputStream().write("HTTP/1.1 400 Bad Request\r\n\r\n".getBytes(US_ASCII));
+            return;
+        }
+        var path = matcher.group(1);
+        if (path.equals("/")) {
             client.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes(US_ASCII));
+            return;
+        } else {
+            client.getOutputStream().write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes(US_ASCII));
+            return;
+        }
+    }
+
+    static void loop(ServerSocket server) {
+        while (true) {
+            try (var client = server.accept()) {
+                handle(client);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 
     public static void main(String[] args) {
         try (ServerSocket serverSocket = new ServerSocket(4221)) {
             serverSocket.setReuseAddress(true);
-            handle(serverSocket.accept());
+            loop(serverSocket);
         } catch (Exception e) {
             die(e);
         }
