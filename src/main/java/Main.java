22b40f61c9045659fb59ffc986ef87d1bd05b279
Daniel Connelly <dhconnelly@gmail.com>
2024-05-06 19:24:19 +0200

    200

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,27 @@
+import static java.nio.charset.StandardCharsets.US_ASCII;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
+    private static final void die(Exception e) {
+        e.printStackTrace(System.err);
+        System.exit(1);
+    }
+
+    static void handle(Socket client) throws IOException {
+        try (client) {
+            client.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes(US_ASCII));
+        }
+    }
+
     public static void main(String[] args) {
-        // You can use print statements as follows for debugging, they'll be visible
-        // when running tests.
-        System.out.println("Logs from your program will appear here!");
-
-        // Uncomment this block to pass the first stage
-
-        ServerSocket serverSocket = null;
-        Socket clientSocket = null;
-
-        try {
-            serverSocket = new ServerSocket(4221);
+        try (ServerSocket serverSocket = new ServerSocket(4221)) {
             serverSocket.setReuseAddress(true);
-            clientSocket = serverSocket.accept(); // Wait for connection from client.
-            System.out.println("accepted new connection");
-        } catch (IOException e) {
-            System.out.println("IOException: " + e.getMessage());
+            handle(serverSocket.accept());
+        } catch (Exception e) {
+            die(e);
         }
     }
 }
